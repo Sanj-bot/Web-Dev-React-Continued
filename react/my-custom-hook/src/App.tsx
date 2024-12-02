5de82afcb36d0c6b91564e2f19015a704eda439d
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-12-02 08:59:55 +0530

    update

--- react/my-custom-hook/src/App.tsx
+++ react/my-custom-hook/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 import './App.css'
+import ProductList from './components/ProductList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div>hello</div>
+     
+     <ProductList/>
    
     </>
   )
--- react/my-custom-hook/src/components/ProductList.tsx
+++ react/my-custom-hook/src/components/ProductList.tsx
@@ -1,9 +1,31 @@
+import { useEffect,useState} from "react";
+import ProductCard from "./ProductCard";
 const ProductList =()=>{
+    const [products,setProducts]=useState([]);
+    useEffect(()=>{
+        console.log("inside useEffect....")
+            getProducts();
+    },[])
+    const getProducts=async ()=>{
+        const ProductList=await fetch('https://fakestoreapi.com/products');
+        const ProductListJson=await ProductList.json();
+        console.log(ProductListJson);
+        setProducts(ProductListJson)
+    }
+    console.log("rendering.....");
+
     return(
-        <div>
+        <div class="productList">
+                
+                {
+                    // products.map(product=><div > {product.title} </div>)
+                   products.map(product=><ProductCard title={product.title} imageUrl={product.image} price={product.price}/>)  
+                }
 
         </div>
     )
 }
 
 export default ProductList;
+
+
