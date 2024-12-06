f95941c0e7b5c4e10a81790cf7c503cdc550cc0d
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-12-06 07:51:30 +0530

    update

--- react/context-api/theme-switcher/my-project/src/App.jsx
+++ react/context-api/theme-switcher/my-project/src/App.jsx
@@ -1,7 +1,27 @@
-export default function App() {
+import React, { useState } from "react";
+import './App.css';
+import { ThemeProvider } from "./contexts/theme";
+function App() {
+  const [themeMode,setThemeMode]=useState;
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    
+  <ThemeProvider>
+    <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* theme button */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/* Card */}
+                    </div>
+                </div>
+    </div>
+  </ThemeProvider>
+    </>
   )
 }
+
+export default App;
