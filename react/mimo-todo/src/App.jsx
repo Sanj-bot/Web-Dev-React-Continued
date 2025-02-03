1b29da05aac341285a6d0e0dde2016c7b3d8a6cf
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-03 21:32:32 +0530

    final commit

--- react/mimo-todo/src/App.jsx
+++ react/mimo-todo/src/App.jsx
@@ -1,47 +1,77 @@
-import { useState } from 'react'
-import {TodoProvider} from './contexts';
+import { useState, useEffect } from 'react'
+import {TodoProvider} from './contexts'
 import './App.css'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
-  const [todos,setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
+
   const addTodo = (todo) => {
-    setTodos((prev)=>[{id:Date.now(),...todo},...prev]);
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
   }
 
-  const updateTodo = (id,todo) => {
-    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id?todo:prevTodo)));
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
 
+    
   }
-
 
   const deleteTodo = (id) => {
-    setTodos((prev)=>prev.filter((todo)=>todo.id!==id));
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
 
-const toggleComplete = (id) => {
-  setTodos((prev)=>prev.map((prevTodo)=>prevTodo===id?{...prevTodo,completed:!prevTodo.completed} :prevTodo));
-}
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
+      try {
+        const parsedTodos = JSON.parse(todos);
+        if (Array.isArray(parsedTodos)) {
+          setTodos(parsedTodos);
+        }
+      } catch (e) {
+        console.error("Failed to parse todos from localStorage", e);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
 
 
 
   return (
-   
-   <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
-
-<div className="bg-[#172842] min-h-screen py-8">
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#ac8a83] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Mimo's Todo 🦁</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
-
-
-   </TodoProvider>
+    </TodoProvider>
   )
 }
 
--- react/mimo-todo/src/contexts/TodoContext.js
+++ react/mimo-todo/src/contexts/TodoContext.js
@@ -1,26 +1,22 @@
-import { createContext,useContext } from "react";
-export const TodoContext = createContext();
+import {createContext, useContext} from "react"
 
-export const TodoContext=createContext(
-    {
-        todos:[
-            {
-                id:1,
-                todo:"Todo msg",
-                completed:false,
-            },{},{}
-        ],
-        addTodo:(todo)=>{},
-        updateTodo:(id,todo)=>{},
-        deleteTodo:(id)=>{},
-        toggleComplete:(id)=>{}
-        
-    }
-);
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 
-export const useTodo=()=>{
-    return useContext(TodoContext);
+export const useTodo = () => {
+    return useContext(TodoContext)
 }
 
-export const TodoProvider = TodoContext.Provider;
+export const TodoProvider = TodoContext.Provider
