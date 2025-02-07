f6786a3140a4ff37c81385943ac9265cf28fa0db
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-07 10:18:43 +0530

    updated

--- react/mimo-blog/src/App.jsx
+++ react/mimo-blog/src/App.jsx
@@ -3,8 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  console.log(Process.env.REACT_APP_APPWRITE_URL)
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
 
   return (
   <>
