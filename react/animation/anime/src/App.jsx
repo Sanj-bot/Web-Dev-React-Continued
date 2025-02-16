bfdcd32d757ec83a9ed64953e77afa1981ad3686
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-16 08:24:58 +0530

    update

--- react/animation/anime/src/App.jsx
+++ react/animation/anime/src/App.jsx
@@ -5,19 +5,23 @@ const App = () => {
   return (
     <div>
       <motion.img 
-       animate={{
-        y:-1000,
-        
+      animate={{
+        x:[0,200]
       }}
       transition={{
         duration:3,
         delay:2
       }}
+      whileHover={{
+        scale:2
+      }}
       src="https://www.svgrepo.com/show/530572/accelerate.svg"
       >
-        
+      
       </motion.img>
-
+      
+        
+   
       
         
      
